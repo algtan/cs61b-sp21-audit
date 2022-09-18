9647521a6a9b34fae4753d1f6aa9cf9b18f81f87
Allen Tan <algtan.dev@gmail.com>
2022-09-18 15:12:13 -0700

    Add solution to HW0, Exercise 3

---
+++ hw0/Exercise3.java
@@ -0,0 +1,19 @@
+public class Exercise3 {
+    public static int max(int[] m) {
+        int currentMax = m[0];
+
+        for (int i = 0; i < m.length; i++) {
+            if (m[i] > currentMax) {
+                currentMax = m[i];
+            }
+        }
+
+        return currentMax;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+
+        System.out.println(max(numbers));
+    }
+}
