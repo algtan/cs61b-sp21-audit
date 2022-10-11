bd1aa3d7852f5ed82df165b906317ddafbd82abf
Allen Tan <algtan.dev@gmail.com>
2022-10-11 13:45:25 -0700

    Create script to split projects and push to Github

---
+++ split_project_into_repo.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SUBDIR=$1
+
+if [ $SUBDIR = "proj0" ]
+then
+    NEWDIR="game2048"
+elif [ $SUBDIR = "proj1" ]
+then
+    NEWDIR="guitar-hero"
+elif [ "$SUBDIR" = "proj2" ]
+then
+    NEWDIR="gitlet"
+elif [ $SUBDIR = "proj3" ]
+then
+    NEWDIR="build-your-own-world"
+else
+    echo "$SUBDIR is not a valid argument"
+    exit
+fi
+
+cd ..
+rm -rf $NEWDIR
+mkdir $NEWDIR
+cd $NEWDIR
+git clone https://github.com/algtan/cs61b-sp21-audit.git .
+git filter-repo --subdirectory-filter $SUBDIR
+git remote add origin "https://github.com/algtan/${NEWDIR}.git"
+git push --force origin main
