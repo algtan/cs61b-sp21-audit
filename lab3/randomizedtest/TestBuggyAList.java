b11a7e72e9e678dbcba32a1d63eee62a2d43aa5b
Allen Tan <algtan.dev@gmail.com>
2022-11-22 16:44:39 -0800

    Add simple comparison test for lab 3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,22 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        correct.addLast(4);
+        buggy.addLast(4);
+        correct.addLast(5);
+        buggy.addLast(5);
+        correct.addLast(6);
+        buggy.addLast(6);
+
+        assertEquals(correct.size(), buggy.size());
+
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+    }
 }
