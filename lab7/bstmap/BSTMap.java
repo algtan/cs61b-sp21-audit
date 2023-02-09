cac569542e0b4eb902f7f4cb6a744a9cc2461619
Allen Tan <algtan.dev@gmail.com>
2023-02-08 22:41:13 -0800

    Create private BSTNode class

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -3,7 +3,48 @@ package bstmap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K, V>  implements Map61B<K, V> {
+public class BSTMap<K extends Comparable<K>, V>  implements Map61B<K, V> {
+    private int size = 0;
+
+    private BSTNode tree;
+
+    private class BSTNode {
+
+        /** Stores KEY as the key in this key-value pair, VAL as the value,
+         *  LEFT as the left node, and RIGHT as the right node in the tree. */
+        BSTNode(K k, V v, BSTNode l, BSTNode r) {
+            key = k;
+            val = v;
+            left = l;
+            right = r;
+        }
+
+        /** Returns the BSTNode in this tree of key-value pairs whose key
+         *  is equal to KEY, or null if no such BSTNode exists. */
+        BSTNode get(K k) {
+            if (k != null && k.equals(key)) {
+                return this;
+            }
+            if (left != null && k.compareTo(key) < 0) {
+                return left.get(k);
+            }
+            if (right != null && k.compareTo(key) > 0) {
+                return right.get(k);
+            }
+            return null;
+        }
+
+        /** Stores the key of the key-value pair of this node in the tree. */
+        K key;
+        /** Stores the value of the key-value pair of this node in the tree. */
+        V val;
+        /** Stores the left BSTNode in the tree. */
+        BSTNode left;
+        /** Stores the right BSTNode in the tree. */
+        BSTNode right;
+
+    }
+
     /** Removes all of the mappings from this map. */
     @Override
     public void clear() {
